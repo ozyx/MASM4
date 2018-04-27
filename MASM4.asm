d8110bd9eb42bec378be958d9fd6789fbc34dbff
ozyx <jessemazzella@gmail.com>
2018-04-27 00:03:09 -0700

    start on add string, but crashes if string is longer than 5 characters (???)

--- MASM4.asm
+++ MASM4.asm
@@ -27,11 +27,12 @@
     putch         PROTO Near32 stdcall, bChar:byte
 
     HEAP_START = 0
-    HEAP_MAX = 4000000000
+    HEAP_MAX   = 400000000
+    STRING_MAX = 512
 
     .data
-    totalMem DWORD 0
-
+    totalMem    DWORD 0
+    strTotalMem  BYTE "00000000",0
     ;menu prompt
     strHeader0   BYTE "MASM4 TEXT EDITOR",10,"Data Structure Memory Consumption: ",0
     strHeader1   BYTE " bytes",10,"<1> View all strings",10,10,0
@@ -46,6 +47,7 @@
     numInput    DWORD ?
     hHeap      HANDLE ?
     pArray      DWORD ?
+    dLength     DWORD ?
 
 ;***********************
 ;    MACRO PrintMenu   *
@@ -54,6 +56,7 @@
 PrintMenu MACRO
     call Clrscr                         ; Clear the screen
     mWriteString strHeader0             ; Print menu
+    mWriteString strTotalMem            ; Print total bytes
     mWriteString strHeader1             ;
     mWriteString strHeader2             ;
     mWriteString strHeader3             ;
@@ -68,21 +71,30 @@ endm
 ;**********************************
 CreateHeap MACRO
     push eax                                    ; Preserve eax
-    INVOKE HeapCreate, 0, HEAP_START, HEAP_MAX  ; Create heap
-    mov hHeap, eax                            ; Retrieve handle
+    INVOKE HeapCreate, 
+        HEAP_ZERO_MEMORY, 
+        HEAP_START, 
+        HEAP_MAX  ; Create heap
+    mov hHeap, eax                              ; Retrieve handle
     pop eax                                     ; Restore eax
 endm
 
-AllocMem MACRO handle:REQ, bytes:REQ
-    push eax
-    INVOKE HeapAlloc, handle, HEAP_ZERO_MEMORY, bytes
+;**************************************
+;         MACRO AllocMem              *
+; Allocate memory and store in pArray *
+;**************************************
+AllocMem MACRO bytes:REQ
+    push eax                                    ; Preserve eax
+    INVOKE HeapAlloc,                           ; Allocate memory on the heap
+            hHeap, 
+            HEAP_ZERO_MEMORY, dLength
     .IF eax == NULL
-        mWrite "HeapAlloc failed"
-        jmp _end
+        mWrite "HeapAlloc failed"               ; Print error
+        jmp _end                                ; Terminate. (TODO: Grow heap?)
     .ELSE
-        mov pArray, eax
+        mov pArray, eax                         ; Store base address in pArray
     .ENDIF
-    pop eax
+    pop eax                                     ; Restore eax
 endm
 
     .code                               ; begin code
@@ -93,8 +105,9 @@ endm
 ;   and get user's choice.                        *
 ;**************************************************
 main PROC
+_setup:
     CreateHeap                          ; Create heap and store handle in hHeap
-_start:
+_mainmenu:
     PrintMenu                           ; Print the menu
     
     mWriteString strPrompt1             ; Prompt for a menu choice
@@ -102,45 +115,88 @@ _start:
     INVOKE ascint32, ADDR strInput      ; Convert to int for comparison
     MOV numInput, eax                   ; Store in eax
 
-    cmp numInput, 1                          ; View all strings
-    je _start
+    cmp numInput, 1                     ; View all strings
+    je _mainmenu
 
-    cmp numInput, 2                          ; Add a string
+    cmp numInput, 2                     ; Add a string
     je _addString
 
-    cmp numInput, 3                          ; Delete a string
-    je _start
+    cmp numInput, 3                     ; Delete a string
+    je _mainmenu
 
-    cmp numInput, 4                          ; Edit a string
-    je _start
+    cmp numInput, 4                     ; Edit a string
+    je _mainmenu
 
-    cmp numInput, 5                          ; String search
-    je _start
+    cmp numInput, 5                     ; String search
+    je _mainmenu
 
-    cmp numInput, 6                          ; Save file
-    je _start
+    cmp numInput, 6                     ; Save file
+    je _mainmenu
 
     jmp _end
 
 
 _addString:
-    call Crlf
- 
-    mWrite "Enter a selection (a - b): ",0
-    INVOKE getstring, ADDR strInput,1
-    cmp strInput, "a"
-    je _fromKeyboard
-    cmp strInput, "b"
-    je _fromFile
-    jmp _start
+    call Crlf                                    ; Print newline
+    mWrite "Enter a selection (a - b): "         ; Prompt user
+    INVOKE getstring, ADDR strInput,1            ; Get user choice
+    cmp strInput, "a"                            ; If a
+    je _fromKeyboard                             ; Get user input from keyboard
+    cmp strInput, "b"                            ; If b
+    je _fromFile                                 ; Read from file
+    jmp _mainmenu                                ; Anything else, go back to main menu
 
 _fromKeyboard:
-    jmp _start
+    call Crlf                                    ; Print newline
+    mWrite "Enter a string: "                    ; Prompt user
+    INVOKE getstring, ADDR strInput, STRING_MAX  ; Get string in strInput, MAX = 512 BYTES
+    mov ebx, OFFSET strInput                     ; Store string in eax
+    push ebx                                     ; Pass string to String_length
+    call String_length                           ; Get length of string in eax
+    add esp, 4                                   ; Clean up stack
+    mov dLength, eax                             ; Store length in dLength
+    cmp dLength, 0                               ; Is it an empty string?
+    je _mainmenu                                 ; If so, jump back to menu
+    AllocMem dLength                             ; Otherwise, allocate memory
+    jmp _mainmenu                                ; Go to main menu
 _fromFile:
-    jmp _start
+    jmp _mainmenu
 
 _end:
+    INVOKE HeapDestroy, hHeap           ; Destroy the heap!!
     INVOKE ExitProcess,0                ; Exit gracefully
 
 main ENDP
-END main               	                ; End program
+
+;###################################################
+; String_length PROC
+;   Calculate length of string
+; Receives: A string
+; Returns: The length of the string in eax
+;###################################################
+String_length PROC
+x_param EQU [ebp + 8]           ; set macro for x
+        push ebp                ; save base pointer
+        mov ebp, esp            ; set ebp equal to esp
+
+        push esi                ; save stack index pointer
+        push ebx                ; save ebx
+
+        mov eax, 0              ; initialize eax to 0
+        mov ebx, x_param        ; pass our parameter
+        mov esi, 0              ; set stack index pointer to 0
+    startloop:
+        cmp BYTE ptr[ebx+esi],0 ; check if we are at the end of the string
+        je endloop              ; if so, jump to endloop
+        inc eax                 ; else, increment count
+        inc esi                 ; point to next letter
+        jmp startloop           ; loop
+    endloop:
+        pop ebx                 ; restore ebx
+        pop esi                 ; restore esi
+
+        pop ebp                 ; restore base pointer value
+        ret                     ; return -- caller must clean up the stack
+String_length ENDP
+
+END main               	        ; end program
